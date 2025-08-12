1e406637e79fb502c80925012f199106a3bd321d
Sumim_Hasan_Joy <153989902+sumimhasan@users.noreply.github.com>
2025-08-13 01:15:49 +0600

    Update config.js

--- core/config.js
+++ core/config.js
@@ -4,7 +4,7 @@ const path = require('path');
 function loadConfig() {
   const configPath = path.join(process.cwd(), 'dependency.json');
   if (!fs.existsSync(configPath)) {
-    console.error('❌ dependency.json not found in current project.');
+    console.error(' dependency.json not found in current project.');
     process.exit(1);
   }
   return JSON.parse(fs.readFileSync(configPath, 'utf-8'));
